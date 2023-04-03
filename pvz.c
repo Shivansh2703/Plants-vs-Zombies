37e7eae8852aa5e4c1dc7a22e8868199a44b7727
Shivansh Singh <shivanshsingh@Shivanshs-MacBook-Air.local>
2023-04-03 16:57:54 -0400

    Fixed conflicts

--- pvz.c
+++ pvz.c
@@ -52,102 +52,106 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "images.h"
 
 // Begin part3.c code for Lab 7
 
 
-volatile int pixel_buffer_start; // global variable
-volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+// volatile int pixel_buffer_start; // global variable
+// volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
 
-void plot_pixel(int x, int y, short int line_color)
-{
-    *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
-}
+// void plot_pixel(int x, int y, short int line_color)
+// {
+//     *(int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
+// }
 
-void clear_screen(){
-	for(int x = 0; x < RESOLUTION_X; x++){
-		for(int y = 0; y < RESOLUTION_Y; y++){
-			plot_pixel(x,y,0x0000);
-			}
-	}
-}
-// code not shown for clear_screen() and draw_line() subroutines
-void draw_line(int x0, int x1, int y0, int y1, short int colour){
-	bool is_steep = ABS(y1 - y0) > ABS(x1 - x0);
-	if(is_steep){
-		int temp = x0;
-		x0 = y0;
-		y0 = temp;
+// void clear_screen(){
+// 	for(int x = 0; x < RESOLUTION_X; x++){
+// 		for(int y = 0; y < RESOLUTION_Y; y++){
+// 			plot_pixel(x,y,0x0000);
+// 			}
+// 	}
+// }
+// // code not shown for clear_screen() and draw_line() subroutines
+// void draw_line(int x0, int x1, int y0, int y1, short int colour){
+// 	bool is_steep = ABS(y1 - y0) > ABS(x1 - x0);
+// 	if(is_steep){
+// 		int temp = x0;
+// 		x0 = y0;
+// 		y0 = temp;
 		
-		temp = x1;
-		x1 = y1;
-		y1 = temp;
-	}
+// 		temp = x1;
+// 		x1 = y1;
+// 		y1 = temp;
+// 	}
 	
-	if(x0 > x1){
-		int temp = x0;
-		x0 = x1;
-		x1 = temp;
+// 	if(x0 > x1){
+// 		int temp = x0;
+// 		x0 = x1;
+// 		x1 = temp;
 		
-		temp = y1;
-		y1 = y0;
-		y0 = temp;
-	}
-	int y_step = 0;
-	int deltax = x1- x0;
-	int deltay = ABS(y1 - y0);
-	int error = -(deltax / 2);
-	int y = y0;
-	if (y0 < y1){
-		y_step = 1;
-	}
-	else {
-		y_step = -1;
-	}
+// 		temp = y1;
+// 		y1 = y0;
+// 		y0 = temp;
+// 	}
+// 	int y_step = 0;
+// 	int deltax = x1- x0;
+// 	int deltay = ABS(y1 - y0);
+// 	int error = -(deltax / 2);
+// 	int y = y0;
+// 	if (y0 < y1){
+// 		y_step = 1;
+// 	}
+// 	else {
+// 		y_step = -1;
+// 	}
 	
-	for(int x = x0; x < x1; x++){
-		if(is_steep){
-			if(x>0 && y>0 && x<=RESOLUTION_Y && y<=RESOLUTION_X){
-				plot_pixel(y,x,colour);
-			}
-		}
-		else{
-			if(x>0 && y>0 && x<=RESOLUTION_X && y<=RESOLUTION_Y){
-				plot_pixel(x,y,colour);
-			}
-		}
-		error =  error + deltay;
-		if(error > 0){
-			y = y + y_step;
-			error = error - deltax;
-		}
-	}
-}
+// 	for(int x = x0; x < x1; x++){
+// 		if(is_steep){
+// 			if(x>0 && y>0 && x<=RESOLUTION_Y && y<=RESOLUTION_X){
+// 				plot_pixel(y,x,colour);
+// 			}
+// 		}
+// 		else{
+// 			if(x>0 && y>0 && x<=RESOLUTION_X && y<=RESOLUTION_Y){
+// 				plot_pixel(x,y,colour);
+// 			}
+// 		}
+// 		error =  error + deltay;
+// 		if(error > 0){
+// 			y = y + y_step;
+// 			error = error - deltax;
+// 		}
+// 	}
+// }
 
-void wait_for_vsync(){
-	register int status; 
-	*pixel_ctrl_ptr = 1; //start synchronization
-	status = *(pixel_ctrl_ptr +3);
-	while(status&0x01){
-		status = *(pixel_ctrl_ptr +3);
-	}
-}
+// void wait_for_vsync(){
+// 	register int status; 
+// 	*pixel_ctrl_ptr = 1; //start synchronization
+// 	status = *(pixel_ctrl_ptr +3);
+// 	while(status&0x01){
+// 		status = *(pixel_ctrl_ptr +3);
+// 	}
+// }
 
 int main(void)
 {
-    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;	
-    /* set front pixel buffer to start of FPGA On-chip memory */
-    *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the 
-                                        // back buffer
-    /* now, swap the front/back buffers, to set the front buffer location */
-    wait_for_vsync();
-    /* initialize a pointer to the pixel buffer, used by drawing functions */
-    pixel_buffer_start = *pixel_ctrl_ptr;
-    clear_screen(); // pixel_buffer_start points to the pixel buffer
-    /* set back pixel buffer to start of SDRAM memory */
-    *(pixel_ctrl_ptr + 1) = 0xC0000000;
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
-    clear_screen(); // pixel_buffer_start points to the pixel buffer
+    // volatile int * pixel_ctrl_ptr = (int *)0xFF203020;	
+    // /* set front pixel buffer to start of FPGA On-chip memory */
+    // *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the 
+    //                                     // back buffer
+    // /* now, swap the front/back buffers, to set the front buffer location */
+    // wait_for_vsync();
+    // /* initialize a pointer to the pixel buffer, used by drawing functions */
+    // pixel_buffer_start = *pixel_ctrl_ptr;
+    // clear_screen(); // pixel_buffer_start points to the pixel buffer
+    // /* set back pixel buffer to start of SDRAM memory */
+    // *(pixel_ctrl_ptr + 1) = 0xC0000000;
+    // pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+    // clear_screen(); // pixel_buffer_start points to the pixel buffer
+
+
+    printf("%i", (int)sizeof(peashooter_map));
 
     int z = 0;
     for(int i = 10; i < 30; i++){
